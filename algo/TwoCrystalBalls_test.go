328e51f747fd83bd0131b432a48952aada4947ba
Mohamed Allam <meda3gm@gmail.com>
2022-09-08 10:04:04 +0100

    fix

--- algo/TwoCrystalBalls_test.go
+++ algo/TwoCrystalBalls_test.go
@@ -5,17 +5,6 @@ import (
 	"testing"
 )
 
-// test("two crystal balls", function () {
-//     let idx = Math.floor(Math.random() * 10000);
-//     const data = new Array(10000).fill(false);
-
-//     for (let i = idx; i < 10000; ++i) {
-//         data[i] = true;
-//     }
-
-//	    expect(two_crystal_balls(data)).toEqual(idx);
-//	    expect(two_crystal_balls(new Array(821).fill(false))).toEqual(-1);
-//	});
 func AddIntToArray(a []int, n int) []int {
 	for i := 0; i < n; i++ {
 		a = append(a, i)
